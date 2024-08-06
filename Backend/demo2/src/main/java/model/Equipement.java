aba07f32ab361f1e01484c93c6b5ad77f49774db
nadianajib <nadianajib832@gmail.com>
2024-08-06 10:15:21 +0100

    créer class équipement et panne

---
+++ Backend/demo2/src/main/java/model/Equipement.java
@@ -0,0 +1,8 @@
+package model;
+
+public class Equipement {
+    private Long idEquipement;
+    private String nom;
+    private String description;
+    private String état;
+}
--- Backend/demo2/src/main/java/model/Panne.java
+++ Backend/demo2/src/main/java/model/Panne.java
@@ -1,4 +1,11 @@
 package model;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Panne {
+    private int idPanne;
+    private String description;
+    private LocalDate dateSignalement;
+    private  String état ;
 }
