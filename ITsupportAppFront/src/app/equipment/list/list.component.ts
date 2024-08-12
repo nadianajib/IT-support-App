79e1f355df0e1ac73db480880c7c07d6264232ca
nadianajib <nadianajib832@gmail.com>
2024-08-12 12:15:25 +0100

    realiser la methode delete equipement

--- ITsupportAppFront/src/app/equipment/list/list.component.ts
+++ ITsupportAppFront/src/app/equipment/list/list.component.ts
@@ -8,30 +8,39 @@ import { EquipementService } from 'src/app/Service/equipement.service';
   styleUrls: ['./list.component.scss']
 })
 export class EquipmentListComponent implements OnInit {
-onDelete(arg0: any) {
-throw new Error('Method not implemented.');
-}
-equipements: Equipement[] = [];
-errorMessage: string = '';
+  equipements: Equipement[] = [];
+  errorMessage: string = '';
 
-constructor(private equipementService:EquipementService ) {}
+  constructor(private equipementService: EquipementService) {}
 
-ngOnInit(): void {
-  this.loadEquipements();
-}
+  ngOnInit(): void {
+    this.loadEquipements();
+  }
 
-loadEquipements(): void {
-  this.equipementService.getEquipement().subscribe(
-    (data: Equipement[]) => {
-      this.equipements = data;
-    },
-    (error) => {
-      console.error('Failed to load equipements', error);
-      this.errorMessage = 'Une erreur est survenue lors du chargement des équipements.';
+  loadEquipements(): void {
+    this.equipementService.getEquipement().subscribe(
+      (data: Equipement[]) => {
+        this.equipements = data;
+      },
+      (error) => {
+        console.error('Failed to load equipements', error);
+        this.errorMessage = 'Une erreur est survenue lors du chargement des équipements.';
+      }
+    );
+  }
+
+  onDelete(id: number): void {
+    if (confirm('Êtes-vous sûr de vouloir supprimer cet équipement ?')) {
+      this.equipementService.deleteEquipement(id).subscribe({
+        next: () => {
+          console.log('Équipement supprimé avec succès');
+          this.loadEquipements(); // Recharger la liste après suppression
+        },
+        error: (err) => {
+          console.error('Erreur lors de la suppression de l\'équipement', err);
+          this.errorMessage = 'Une erreur est survenue lors de la suppression de l\'équipement.';
+        }
+      });
     }
-  );
-  
-}
-
-
+  }
 }
